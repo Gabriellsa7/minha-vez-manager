0d7a621374fb35985a765b994bb08057cbfb2c47
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-21 21:55:20 -0300

    feat: create API call to user login

---
+++ src/features/login/api/post-login.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useMutation, type UseMutationOptions } from '@tanstack/react-query';
+
+import type { IAuthTokenResponse } from '../../../config/entities/auth/auth.entity';
+
+export const POST_LOGIN_KEY = 'POST_LOGIN_KEY';
+
+export interface PostLoginParams {
+  email: string;
+  password: string;
+}
+
+type PostLoginResponse = IAuthTokenResponse;
+
+const postLogin = async (
+  params: PostLoginParams
+): Promise<PostLoginResponse> => {
+  const path = '/auth/login';
+
+  const response = await axios.post<PostLoginResponse>(path, params);
+
+  return response.data;
+};
+
+export const usePostLogin = (
+  options?: UseMutationOptions<PostLoginResponse, unknown, PostLoginParams>
+) => {
+  return useMutation({
+    mutationFn: postLogin,
+    ...options,
+  });
+};
