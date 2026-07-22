57cee7f62e13c70ff622a95ad8e25475f99c22b7
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-21 22:03:57 -0300

    refactor: refactor api call to get current user

--- src/config/api/get-current-user.ts
+++ src/config/api/get-current-user.ts
@@ -1,26 +1,19 @@
 import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
 import { apiClient } from '../../services/axios';
+import type { IUser } from '../entities/user/user.entity';
 
 const GET_CURRENT_USER = 'GET_CURRENT_USER';
 
-export interface IUser {
-  _id: string;
-  name: string;
-  email: string;
-  role: string;
-}
+export type GetUserParamsResponse = IUser;
 
-const getCurrentUser = async () => {
-  const { data } = await apiClient.get<IUser>('/users/me');
+const getCurrentUser = async (): Promise<GetUserParamsResponse> => {
+  const response = await apiClient.get<GetUserParamsResponse>('/users/me');
 
-  return data;
+  return response.data;
 };
 
 export const useCurrentUser = (
-  options?: Omit<
-    UseQueryOptions<IUser>,
-    'queryKey' | 'queryFn'
-  >
+  options?: Omit<UseQueryOptions<GetUserParamsResponse>, 'queryKey' | 'queryFn'>
 ) => {
   return useQuery({
     queryKey: [GET_CURRENT_USER],
