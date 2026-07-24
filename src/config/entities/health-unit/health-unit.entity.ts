9452469d83882156a7cf46207b1ed0726c877f71
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-24 15:56:56 -0300

    feat: create health units types

---
+++ src/config/entities/health-unit/health-unit.entity.ts
@@ -0,0 +1,33 @@
+export interface IHealthUnit {
+  _id: string;
+  userId: string;
+  name: string;
+  address: IHealthUnitAddress;
+  phone: string;
+  description?: string;
+  services: IService[];
+  email: string;
+  img?: string;
+  createdAt?: Date;
+  updateAt?: Date;
+}
+
+export interface IService {
+  _id: string;
+  name: string;
+  description?: string;
+  duration?: number;
+  price?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface IHealthUnitAddress {
+  street: string;
+  number: string;
+  complement?: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
