8ff67e2810346fdf5f9b96f8a0ca97c6f0a08f4e
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-25 18:46:27 -0300

    feat: create api call to create health profissional

---
+++ src/features/profissionals/components/health-professional-modal/api/post-health-professional.ts
@@ -0,0 +1,32 @@
+import { useMutation, type UseMutationOptions } from '@tanstack/react-query';
+import { apiClient } from '../../../../../services/axios';
+import type { IHealthProfessional } from '../../../../../config/entities/health-profissional/health-professional.entity';
+
+export interface CreateHealthProfessionalParams {
+  userId?: string;
+  healthUnitId: string;
+  name: string;
+  email: string;
+  password: string;
+  specialty: string;
+  professionalLicense: string;
+}
+
+const postHealthProfessional = async (
+  params: CreateHealthProfessionalParams
+): Promise<IHealthProfessional> => {
+  const { data } = await apiClient.post<IHealthProfessional>(
+    '/health-professionals',
+    params
+  );
+
+  return data;
+};
+
+export const usePostHealthProfessional = (
+  options?: UseMutationOptions<
+    IHealthProfessional,
+    unknown,
+    CreateHealthProfessionalParams
+  >
+) => useMutation({ mutationFn: postHealthProfessional, ...options });
