6758d473bfccb617781c9e606bf7aaf01642da4c
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-25 17:41:03 -0300

    feat: create api call to create the health unit

---
+++ src/features/healt-unit-manager/components/health-unit-modal/api/post-health-unit.ts
@@ -0,0 +1,41 @@
+import { useMutation, type UseMutationOptions } from '@tanstack/react-query';
+import { apiClient } from '../../../../../services/axios';
+import type {
+  IHealthUnit,
+  IService,
+} from '../../../../../config/entities/health-unit/health-unit.entity';
+
+export interface CreateHealthUnitParams {
+  userId: string;
+  name: string;
+  phone: string;
+  email: string;
+  description?: string;
+  img?: string;
+  address: {
+    street: string;
+    number: string;
+    complement?: string;
+    neighborhood: string;
+    city: string;
+    state: string;
+    zipCode: string;
+  };
+  services: Array<Omit<IService, '_id' | 'createdAt' | 'updatedAt'>>;
+}
+
+const postHealthUnit = async (
+  params: CreateHealthUnitParams
+): Promise<IHealthUnit> => {
+  const { data } = await apiClient.post<IHealthUnit>('/health-units', params);
+
+  return data;
+};
+
+export const usePostHealthUnit = (
+  options?: UseMutationOptions<IHealthUnit, unknown, CreateHealthUnitParams>
+) =>
+  useMutation({
+    mutationFn: postHealthUnit,
+    ...options,
+  });
