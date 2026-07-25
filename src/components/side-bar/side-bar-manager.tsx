e680e932d058126be8778eba216d5422f60b88fc
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-25 10:29:28 -0300

    feat: create sidebar component

---
+++ src/components/side-bar/side-bar-manager.tsx
@@ -0,0 +1,41 @@
+import { CirclePlus } from 'lucide-react';
+import type { SidebarItem } from '../../config/constants/side-bar-manager/side-bar-manager';
+import style from './side-bar-manager.module.scss';
+import { NavLink } from 'react-router-dom';
+import { Profile } from '../../features/profile/profile';
+import type { IUser } from '../../config/entities/user/user.entity';
+
+interface SideBarProps {
+  items: SidebarItem[];
+  pageTitle: string;
+  user?: IUser;
+}
+
+function SideBar({ items, pageTitle, user }: SideBarProps) {
+  return (
+    <div className={style.container}>
+      <div className={style.cardContainer}>
+        <div className={style.titleContainer}>
+          <CirclePlus size={26} color="#007a78" />
+          <span>{pageTitle}</span>
+        </div>
+        {items.map(({ title, icon: Icon, path }) => (
+          <div key={path} className={style.navCard}>
+            <NavLink
+              to={path}
+              className={({ isActive }) =>
+                isActive ? style.activeItem : style.item
+              }
+            >
+              <Icon size={20} />
+              <span>{title}</span>
+            </NavLink>
+          </div>
+        ))}
+      </div>
+      <Profile user={user} />
+    </div>
+  );
+}
+
+export { SideBar };
