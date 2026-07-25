539df4103f9ca3b62b4ff136e915eec6bad33a7c
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-25 18:43:55 -0300

    style: add new styles

--- src/features/profissionals/profissionals.tsx
+++ src/features/profissionals/profissionals.tsx
@@ -1,22 +1,34 @@
+import { useState } from 'react';
 import { SideBar } from '../../components/side-bar/side-bar-manager';
+import { HeaderManager } from '../../components/header-manager/header-manager';
 import { useCurrentUser } from '../../config/api/get-current-user';
 import { SIDEBAR_MANAGER_ITEMS } from '../healt-unit-manager/constants';
+import { HealthProfessionalModal } from './components/health-professional-modal/health-professional-modal';
 import style from './profissionals.module.scss';
 
 function Professionals() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { data: user } = useCurrentUser();
 
   return (
-    <div className={style.container}>
-      <SideBar
-        items={SIDEBAR_MANAGER_ITEMS}
-        pageTitle="Painel Manager"
-        user={user}
-      />
-      <div>
-        <span>Professionals</span>
+    <>
+      <div className={style.container}>
+        <SideBar
+          items={SIDEBAR_MANAGER_ITEMS}
+          pageTitle="Painel Manager"
+          user={user}
+        />
+        <div className={style.mainContent}>
+          <HeaderManager
+            title="Profissionais"
+            subtitle="Gerencie os profissionais das suas unidades"
+            buttonText="Novo profissional"
+            onButtonClick={() => setIsModalOpen(true)}
+          />
+        </div>
       </div>
-    </div>
+      <HealthProfessionalModal open={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </>
   );
 }
 
