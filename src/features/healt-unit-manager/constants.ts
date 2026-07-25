cc256ade041741c11acdf5dd5494330c215bf8ec
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-25 10:32:41 -0300

    feat: create sidebar manager constants

---
+++ src/features/healt-unit-manager/constants.ts
@@ -0,0 +1,15 @@
+import { Hospital, Stethoscope } from 'lucide-react';
+import type { SidebarItem } from '../../config/constants/side-bar-manager/side-bar-manager';
+
+export const SIDEBAR_MANAGER_ITEMS: SidebarItem[] = [
+  {
+    title: 'Unidades de Saude',
+    icon: Hospital,
+    path: '/',
+  },
+  {
+    title: 'Profissionais',
+    icon: Stethoscope,
+    path: '/profissionals',
+  },
+];
