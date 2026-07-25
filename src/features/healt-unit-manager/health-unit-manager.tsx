beb8131e59a29c9b5772e18de404907a88d1c016
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-25 15:19:34 -0300

    feat: create health unit manager page

--- src/features/healt-unit-manager/health-unit-manager.tsx
+++ src/features/healt-unit-manager/health-unit-manager.tsx
@@ -1,8 +1,43 @@
+import { useState } from 'react';
+import { SideBar } from '../../components/side-bar/side-bar-manager';
+import { useCurrentUser } from '../../config/api/get-current-user';
+import { useHealthUnitsByUserId } from '../../config/api/get-health-units-by-user-id';
+import { SIDEBAR_MANAGER_ITEMS } from './constants';
+import style from './health-unit-manager.module.scss';
+import { HeaderManager } from '../../components/header-manager/header-manager';
+import { HealthUnitModal } from './components/health-unit-modal/health-unit-modal';
+import { HealthUnitCard } from './components/health-unit-card/health-unit-card';
+
 function HealthUnitManager() {
+  const [openModal, setOpenModal] = useState(false);
+  const { data: user } = useCurrentUser();
+
+  const { data: healthUnit } = useHealthUnitsByUserId(user?._id);
+
   return (
-    <div>
-      <span>Health Unit Manager</span>
-    </div>
+    <>
+      <div className={style.container}>
+        <SideBar
+          items={SIDEBAR_MANAGER_ITEMS}
+          pageTitle="Painel Manager"
+          user={user}
+        />
+        <div className={style.mainContent}>
+          <HeaderManager
+            title="Unidade de Saúde"
+            subtitle="Gerencie suas unidades de Saúde"
+            buttonText="Nova Unidade"
+            onButtonClick={() => setOpenModal(true)}
+          />
+          <div className={style.healthUnitSection}>
+            {healthUnit?.map((health) => (
+              <HealthUnitCard key={health._id} healthUnit={health} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <HealthUnitModal onClose={() => setOpenModal(false)} open={openModal} />
+    </>
   );
 }
 
