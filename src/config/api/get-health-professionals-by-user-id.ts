3ab0540cab00253c065ac82b7b36e30c064ff971
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-25 19:55:33 -0300

    feat: create api call to get professional by user id

---
+++ src/config/api/get-health-professionals-by-user-id.ts
@@ -0,0 +1,37 @@
+import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
+import { apiClient } from '../../services/axios';
+import type { IHealthProfessional } from '../entities/health-profissional/health-professional.entity';
+
+const GET_HEALTH_PROFESSIONALS_BY_USER_ID =
+  'GET_HEALTH_PROFESSIONALS_BY_USER_ID';
+
+export type GetHealthProfessionalsByUserId = IHealthProfessional[];
+
+const getHealthProfessionalsByUserId = async (
+  userId: string
+): Promise<GetHealthProfessionalsByUserId> => {
+  const path = `/health-professionals/user/${userId}`;
+
+  try {
+    const response = await apiClient.get<GetHealthProfessionalsByUserId>(path);
+
+    return response.data;
+  } catch {
+    throw new Error(path);
+  }
+};
+
+export const useHealthProfessionalsByUserId = (
+  userId: string | undefined,
+  options?: Omit<
+    UseQueryOptions<GetHealthProfessionalsByUserId>,
+    'queryKey' | 'queryFn'
+  >
+) => {
+  return useQuery({
+    queryKey: [GET_HEALTH_PROFESSIONALS_BY_USER_ID, userId],
+    queryFn: () => getHealthProfessionalsByUserId(userId!),
+    enabled: Boolean(userId),
+    ...options,
+  });
+};
