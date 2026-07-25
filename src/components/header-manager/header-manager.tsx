e1a4503c9088e7ad4516292a607e41d4f5f2f1f1
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-25 15:19:12 -0300

    feat: create health unit manager header

---
+++ src/components/header-manager/header-manager.tsx
@@ -0,0 +1,34 @@
+import { Bell, Plus } from 'lucide-react';
+import style from './header-manager.module.scss';
+
+interface HeaderManagerProps {
+  title: string;
+  subtitle: string;
+  buttonText: string;
+  onButtonClick: () => void;
+}
+
+function HeaderManager({
+  buttonText,
+  onButtonClick,
+  subtitle,
+  title,
+}: HeaderManagerProps) {
+  return (
+    <div className={style.container}>
+      <div className={style.titleContainer}>
+        <span className={style.title}>{title}</span>
+        <span className={style.subtitle}>{subtitle}</span>
+      </div>
+      <div className={style.buttonSection}>
+        <Bell size={22} color="#007a78" />
+        <div className={style.button}>
+          <Plus size={20} color="#FFFFFF" />
+          <button onClick={onButtonClick}>{buttonText}</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { HeaderManager };
