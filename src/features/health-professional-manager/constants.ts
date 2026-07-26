c9d03b5c03478889d62eb706a17bce1a93622ae1
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-26 09:10:30 -0300

    feat: create sidebar professional constants

---
+++ src/features/health-professional-manager/constants.ts
@@ -0,0 +1,25 @@
+import type { SidebarItem } from '../../config/constants/side-bar-manager/side-bar-manager';
+import { Calendar, CircleUserRound, History, MonitorCog } from 'lucide-react';
+
+export const SIDEBAR_PROFESSIONAL_MANAGER: SidebarItem[] = [
+  {
+    title: 'Fila Ativa',
+    icon: MonitorCog,
+    path: '/health-professional-manager',
+  },
+  {
+    title: 'Histórico',
+    icon: History,
+    path: '/history',
+  },
+  {
+    title: 'Agendamento',
+    icon: Calendar,
+    path: '/schedule',
+  },
+  {
+    title: 'Perfil',
+    icon: CircleUserRound,
+    path: '/profile',
+  },
+];
