6a1c3a9bcf769b2f399ff86fcc76ea4ed7b93ace
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-08-02 10:41:08 -0300

    feat: create constants util

---
+++ src/features/healt-unit-manager/components/health-unit-modal/constants.ts
@@ -0,0 +1,11 @@
+import type { WeekDay } from '../../../../config/entities/health-unit/health-unit.entity';
+
+export const weekDayLabel: Record<WeekDay, string> = {
+  MONDAY: 'Segunda-feira',
+  TUESDAY: 'Terça-feira',
+  WEDNESDAY: 'Quarta-feira',
+  THURSDAY: 'Quinta-feira',
+  FRIDAY: 'Sexta-feira',
+  SATURDAY: 'Sábado',
+  SUNDAY: 'Domingo',
+};
