6a3aa6a5ba243dadc8dd75fe0a4c5cfd4e9aa11e
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-08-02 08:54:50 -0300

    feat: not logout when press f5

--- src/routes/utils/authentication-outlet.tsx
+++ src/routes/utils/authentication-outlet.tsx
@@ -2,7 +2,11 @@ import { Navigate, Outlet } from 'react-router';
 import { useAuth } from '../../hooks/use-auth';
 
 function AuthenticationOutlet() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return null;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
